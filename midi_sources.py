d0fb22921fbd8f834706a25c3484d4b17da2f024
Miriam Ruiz <miriam@debian.org>
2019-10-11 15:21:29 +0200

    Added midi support to test

---
+++ midi_sources.py
@@ -0,0 +1,179 @@
+import fluidsynth
+import rtmidi
+import mido
+import time
+
+class RandomSoundPlayer():
+    def __init__(self, keyboard_handlers=None):
+        self.keyboard_handlers = keyboard_handlers
+        self.fs = fluidsynth.Synth()
+        self.fs.start(driver="alsa")
+        print("FluidSynth Started")
+        self.sfid = self.fs.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")
+        self.fs.program_select(0, self.sfid, 0, 0)
+    def __del__(self): # See:https://eli.thegreenplace.net/2009/06/12/safely-using-destructors-in-python/
+        self.fs.delete()
+        print("FluidSynth Closed")
+        del self.fs
+    def press(self, key, velocity=64, duration=0.5):
+        self.fs.noteon(0, key + 19, velocity)
+        if self.keyboard_handlers:
+            for keyboard_handler in self.keyboard_handlers:
+                keyboard_handler.press(key + 19, 1, True)
+        time.sleep(duration)
+        self.fs.noteoff(0, key + 19)
+        if self.keyboard_handlers:
+            for keyboard_handler in self.keyboard_handlers:
+                keyboard_handler.press(key + 19, 1, False)
+    @staticmethod
+    def random_key(mean_key=44):
+        x = random.gauss(mean_key, 10.0)
+        if x < 1: x = 1
+        elif x > 88: x = 88
+        return int(round(x))
+    @staticmethod
+    def random_velocity():
+        x = random.gauss(100.0, 10.0)
+        if x < 1: x = 1
+        elif x > 127: x = 127
+        return int(round(x))
+    @staticmethod
+    def random_duration(self, mean_duration=2.0):
+        x = random.gauss(mean_duration, 2.0)
+        if x < 0.2: x = 0.2
+        return x
+    def random_play(self, num, mean_key, mean_duration):
+        while num != 0:
+            num -= 1
+            key = self.random_key(mean_key)
+            velocity = self.random_velocity()
+            duration = self.random_duration(mean_duration)
+            self.press(key, velocity, duration)
+        #if self.keyboard_handler: self.keyboard_handler.show(False)
+
+class MidiFileSoundPlayer():
+    def __init__(self, filename, keyboard_handlers=None):
+        self.keyboard_handlers = keyboard_handlers
+        self.fs = fluidsynth.Synth()
+        self.fs.start(driver="alsa")
+        print("FluidSynth Started")
+        self.sfid = self.fs.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")
+        for channel in range(0, 16):
+            self.fs.program_select(channel, self.sfid, 0, 0)
+        self.midi_file = mido.MidiFile(filename)
+        print('Midi File: {}'.format(self.midi_file.filename))
+        length = self.midi_file.length
+        print('Song length: {} minutes, {} seconds'.format(int(length / 60), int(length % 60)))
+        print('Tracks:')
+        for i, track in enumerate(self.midi_file.tracks):
+            print('  {:2d}: {!r}'.format(i, track.name.strip()))
+
+    def play(self):
+        time.sleep(1)
+        for message in self.midi_file.play(meta_messages=True):
+            #sys.stdout.write(repr(message) + '\n')
+            #sys.stdout.flush()
+            if isinstance(message, mido.Message):
+                if message.type == 'note_on':
+                    self.fs.noteon(message.channel, message.note, message.velocity)
+                    if self.keyboard_handlers:
+                        for keyboard_handler in self.keyboard_handlers:
+                            keyboard_handler.press(message.note, message.channel, True)
+
+                elif message.type == 'note_off':
+                    self.fs.noteoff(message.channel, message.note)
+                    if self.keyboard_handlers:
+                        for keyboard_handler in self.keyboard_handlers:
+                            keyboard_handler.press(message.note, message.channel, False)
+
+            elif message.type == 'set_tempo':
+                #print('Tempo changed to {:.1f} BPM.'.format(mido.tempo2bpm(message.tempo)))
+                pass
+
+    def __del__(self): # See:https://eli.thegreenplace.net/2009/06/12/safely-using-destructors-in-python/
+        self.fs.delete()
+        print("FluidSynth Closed")
+        del self.fs
+
+class RtMidiSoundPlayer():
+    def __init__(self, keyboard_handlers=None):
+        self.keyboard_handlers = keyboard_handlers
+        self.fs = fluidsynth.Synth()
+        self.fs.start(driver="alsa")
+        print("FluidSynth Started")
+        self.sfid = self.fs.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")
+        self.fs.program_select(0, self.sfid, 0, 0)
+
+        self.midi_in = rtmidi.MidiIn()
+        available_ports = self.midi_in.get_ports()
+        if available_ports:
+            midi_port_num = 1
+            try:
+                self.midi_in_port = self.midi_in.open_port(midi_port_num)
+            except rtmidi.InvalidPortError:
+                print("Failed to open MIDI input")
+                self.midi_in_port = None
+                return
+            print("Using MIDI input Interface {}: '{}'".format(midi_port_num, available_ports[midi_port_num]))
+        else:
+            print("Creating virtual MIDI input.")
+            self.midi_in_port = self.midi_in.open_virtual_port("midi_driving_in")
+
+        self.midi_in.set_callback(self.midi_received)
+
+    def __del__(self): # See:https://eli.thegreenplace.net/2009/06/12/safely-using-destructors-in-python/
+        self.fs.delete()
+        print("FluidSynth Closed")
+        del self.fs
+
+    def midi_received(self, midi_event, data=None):
+        current_timestamp = time.time_ns() / (10 ** 9) # Converted to floating-point seconds
+        midi_msg, delta_time = midi_event
+        if len(midi_msg) > 2:
+            pressed = (midi_msg[2] != 0)
+            note = midi_msg[1]
+            pitch_class = midi_msg[1] % 12
+            octave = midi_msg[1] // 12
+
+            #print("%s" % ((pressed, note, octave, pitch_class),))
+
+            if pressed: # A note was hit
+                if self.keyboard_handlers:
+                    for keyboard_handler in self.keyboard_handlers:
+                        keyboard_handler.press(midi_msg[1], 16, True)
+
+            else: # A note was released
+                if self.keyboard_handlers:
+                    for keyboard_handler in self.keyboard_handlers:
+                        keyboard_handler.press(midi_msg[1], 16, False)
+
+class FifoList():
+    def __init__(self):
+        self.data = {}
+        self.nextin = 0
+        self.nextout = 0
+        self.lock = Lock()
+    def append(self, data):
+        try:
+            self.lock.acquire()
+            self.nextin += 1
+            self.data[self.nextin] = data
+        finally:
+            self.lock.release()
+    def pop(self):
+        try:
+            self.lock.acquire()
+            self.nextout += 1
+            result = self.data[self.nextout]
+            del self.data[self.nextout]
+        finally:
+            self.lock.release()
+        return result
+    def peek(self):
+        try:
+            self.lock.acquire()
+            result = self.data[self.nextout + 1] if self.data else None
+        finally:
+            self.lock.release()
+        return result
+
--- test.py
+++ test.py
@@ -3,45 +3,51 @@
 
 import cairo
 import math
+import os
 import queue
 import sys
+
+from midi_sources import MidiFileSoundPlayer, RtMidiSoundPlayer
+from threading import Thread, Lock
 
 SCALE_MAJOR_DIATONIC = (1<<0) + (1<<2) + (1<<4) + (1<<6) + (1<<7) + (1<<9) + (1<<11)
 SCALE_MAJOR_MELODIC  = (1<<0) + (1<<2) + (1<<4) + (1<<6) + (1<<7) + (1<<9) + (1<<10)
 
-#MAIN_TRIADS = [
-#    [(0, 0, 0), (4, 0, -1), (7, 1,  0)], # Major triad
-#    [(0, 0, 0), (3, 1,  1), (7, 1,  0)], # Minor triad
-#    [(0, 0, 0), (4, 0, -1), (8, 0, -2)], # Augmented triad
-#    [(0, 0, 0), (3, 1,  1), (6, 2,  2)], # Diminished triad
-#]
-
 CHORDS_INFO = [
     # Tertian seventh chords: constructed using a sequence of major thirds and/or minor thirds
-    [ [], ((0, 0, 0), (4, 0, -1), (7, 1,  0), (11, 1, -1)), "Major seventh Chord" ],
-    [ [], ((0, 0, 0), (3, 1,  1), (7, 1,  0), (10, 2,  1)), "Minor seventh Chord" ],
-    [ [], ((0, 0, 0), (4, 0, -1), (7, 1,  0), (10, 2,  1)), "Dominant seventh Chord" ],
-    [ [], ((0, 0, 0), (3, 1,  1), (6, 2,  2), (9,  3,  0)), "Diminished seventh Chord" ],
-    [ [], ((0, 0, 0), (3, 1,  1), (6, 2,  2), (10, 2,  1)), "Half-diminished seventh Chord" ],
-    [ [], ((0, 0, 0), (3, 1,  1), (7, 1,  0), (11, 1, -1)), "Minor major seventh Chord" ],
-    [ [], ((0, 0, 0), (4, 0, -1), (8, 0, -2), (11, 1, -1)), "Augmented major seventh Chord" ],
+    [ [], (0, 4, 7, 11), "Major seventh Chord" ],
+    [ [], (0, 3, 7, 10), "Minor seventh Chord" ],
+    [ [], (0, 4, 7, 10), "Dominant seventh Chord" ],
+    [ [], (0, 3, 6,  9), "Diminished seventh Chord" ],
+    [ [], (0, 3, 6, 10), "Half-diminished seventh Chord" ],
+    [ [], (0, 3, 7, 11), "Minor major seventh Chord" ],
+    [ [], (0, 4, 8, 11), "Augmented major seventh Chord" ],
 
     # Non-tertian seventh chords: constructed using augmented or diminished thirds
-    #[ [], ((0, 0, 0), (4, 0, -1), (8, 0, -2), (10, 2, -2)), "Augmented minor seventh Chord" ],
-    #[ [], ((0, 0, 0), (3, 1,  1), (6, 2,  2), (11, 1, -1)), "Diminished major seventh Chord" ],
-    #[ [], ((0, 0, 0), (4, 0, -1), (6, 2, -1), (10, 2,  1)), "Dominant seventh flat five Chord" ],
-    #[ [], ((0, 0, 0), (4, 0, -1), (6, 2,  2), (11, 1, -1)), "Major seventh flat five Chord" ],
+    [ [], (0, 4, 8, 10), "Augmented minor seventh Chord" ],
+    [ [], (0, 3, 6, 11), "Diminished major seventh Chord" ],
+    [ [], (0, 4, 6, 10), "Dominant seventh flat five Chord" ],
+    [ [], (0, 4, 6, 11), "Major seventh flat five Chord" ],
 
     # Primary triads
-    [ [], ((0, 0, 0), (4, 0, -1), (7, 1,  0)),  "Major Triad" ],
-    [ [], ((0, 0, 0), (3, 1,  1), (7, 1,  0)),  "Minor Triad" ],
-    [ [], ((0, 0, 0), (3, 1,  1), (6, 2,  2)),  "Diminished Triad" ],
-    [ [], ((0, 0, 0), (4, 0, -1), (8, 0, -2)),  "Augmented Triad" ],
+    [ [], (0, 4, 7),  "Major Triad" ],
+    [ [], (0, 3, 7),  "Minor Triad" ],
+    [ [], (0, 3, 6),  "Diminished Triad" ],
+    [ [], (0, 4, 8),  "Augmented Triad" ],
 
-    # Suspended triads
-    #[ [], ((0, 0, 0), (2,  2, 0), (7, 1,  0)),  "Sus2 Triad" ],
-    #[ [], ((0, 0, 0), (5, -1, 0), (7, 1,  0)),  "Sus4 Triad" ],
+#    # Suspended triads
+    [ [], (0, 2, 7),  "Sus2 Triad" ],
+    [ [], (0, 5, 7),  "Sus4 Triad" ],
 ]
+
+for chord_info in CHORDS_INFO:
+    if not chord_info[0]:
+        chord_info[0] = [0] * 12
+        for i in range(0, 12):
+            chord_mask = 0
+            for num_note in chord_info[1]:
+                chord_mask |= 1 << (i + num_note) % 12
+            chord_info[0][i] = chord_mask
 
 NOTE_NAMES = ['I', 'ii', 'II', 'iii', 'III', 'IV', 'v', 'V', 'vi', 'VI', 'vii', 'VII']
 
@@ -61,18 +67,33 @@ def seq_floats(start, stop, step=1):
     else:
         return([])
 
+fast = True
+
+if fast:
+    import OpenGL
+    OpenGL.ERROR_CHECKING = False
+    OpenGL.ERROR_LOGGING = False
+    OpenGL.ERROR_ON_COPY = True
+    OpenGL.STORE_POINTERS = False
+
 class TestPic:
     def __init__(self, D, scale=SCALE_MAJOR_DIATONIC, tonic=0):
         self.ctx = None
 
         self.notes = [(scale & 1<<(r%12) != 0) for r in range(tonic*7, tonic*7 + 12)]
 
+        for chord_signatures, chord_intervals, chord_name in CHORDS_INFO:
+            for num_signature, chord_signature in enumerate(chord_signatures):
+                if (scale & chord_signature) == chord_signature:
+                    print("Chord: {} on {}".format(chord_name, NOTE_NAMES[(num_signature) % 12]))
+
         self.width = 1200
         self.height = 800
         self.vstep = 14. * math.sqrt(5.)
         self.hstep = 14.
 
-        self.root = NOTE_MIDI_C4
+        self.base_note = NOTE_MIDI_C4 - 12 * 2
+        self.pressed_notes = [ 0 ] * 128
 
     def check_chord(self, chord, note=(0,0,0)):
         base_note, base_x, base_y = note
@@ -121,18 +142,32 @@ class TestPic:
             return (0.9, 0.9, 0.9)
         return self.hsv_to_rgb(360. * ((note*7)%12)/12., saturation, value)
 
-    def draw_note(self, note, x, y):
-        note = note % 12
+    def draw_note(self, n):
+        note = (self.base_note + n) % 12
+        x = self.hstep * (1 + n) - self.width / 2.
+        y = self.height / 2 - self.hstep - ((n * 7 + 4) % 24) * self.vstep
+
+        is_pressed = (self.pressed_notes[self.base_note + n] != 0)
+
         self.ctx.save()
 
-        color = self.get_color_from_note(note, 1. if self.notes[note] else 0.1)
+        if self.notes[note]:
+            color = self.get_color_from_note(note, 1.)
+        else:
+            color = self.get_color_from_note(note, .1)
         self.ctx.set_source_rgb(*color)
         self.ctx.arc(x, y, 12, 0, 2. * math.pi)
         self.ctx.fill()
 
-        self.ctx.set_source_rgb(0.3, 0.3, 0.3)
+        if self.notes[note] or is_pressed:
+            self.ctx.set_source_rgb(0.3, 0.3, 0.3)
+        else:
+            self.ctx.set_source_rgb(0.6, 0.6, 0.6)
         self.ctx.arc(x, y, 12, 0, 2. * math.pi)
-        self.ctx.set_line_width(2.0)
+        if is_pressed:
+            self.ctx.set_line_width(4.0)
+        else:
+            self.ctx.set_line_width(2.0)
         self.ctx.stroke()
 
         label = NOTE_NAMES[note]
@@ -154,37 +189,46 @@ class TestPic:
 
         self.ctx.translate(self.width // 2, self.height // 2)
 
-        for note in range(int(self.width / self.hstep) - 1):
-            x = self.hstep * (1 + note) - self.width / 2.
-            self.ctx.set_source_rgb(0.8, 0.8, 0.8)
+        for n in range(int(self.width / self.hstep) - 1):
+            note = self.base_note + n
+            channel = self.pressed_notes[note].bit_length() - 1
+            x = self.hstep * (1 + n) - self.width / 2.
+            if channel >= 0:
+                self.ctx.set_source_rgb(*self.hsv_to_rgb(360. * ((channel*9)%16) / 16., 1.0, 0.6))
+            else:
+                self.ctx.set_source_rgb(0.8, 0.8, 0.8)
             self.ctx.move_to(x, self.height/2)
             self.ctx.line_to(x, -self.height/2)
             self.ctx.stroke()
 
-        for note in range(24):
-            y = self.height / 2 - self.hstep - ((note * 7 + 4) % 24) * self.vstep
-            color = self.get_color_from_note(note, 1. if self.notes[note % 12] else 0.05)
+        for n in range(24):
+            note = self.base_note + n
+            y = self.height / 2 - self.hstep - ((n * 7 + 4) % 24) * self.vstep
+            color = self.get_color_from_note(note, 1. if self.notes[note % 12] else 0.1)
             self.ctx.set_source_rgb(*color)
             self.ctx.move_to(self.width/2, y)
             self.ctx.line_to(-self.width/2, y)
             self.ctx.stroke()
 
-        for note in range(int(self.width / self.hstep) - 1):
-            x = self.hstep * (1 + note) - self.width / 2.
-            y = self.height / 2 - self.hstep - ((note * 7 + 4) % 24) * self.vstep
-            self.draw_note(note, x, y)
+        for n in range(int(self.width / self.hstep) - 1):
+            self.draw_note(n)
+
+    def press(self, num_key, channel, action=True):
+        if action:
+            self.pressed_notes[num_key] |= 1<<channel
+        else:
+            self.pressed_notes[num_key] &= ~(1<<channel)
 
 def main():
     import ctypes
     import time
-
-    from pyglet import app, clock, font, gl, image, window
+    import pyglet
 
     from MusicDefs import MusicDefs
 
     pic = TestPic(D=100, scale=SCALE_MAJOR_DIATONIC, tonic=1)
 
-    window = window.Window(width=pic.width, height=pic.height)
+    window = pyglet.window.Window(width=pic.width, height=pic.height, resizable=True)
     #ft = font.load('Arial', 24)
     #text = font.Text(ft, 'Hello World')
 
@@ -192,38 +236,98 @@ def main():
     data = (ctypes.c_ubyte * (pic.width * pic.height * 4))()
     stride = pic.width * 4
     surface = cairo.ImageSurface.create_for_data (data, cairo.FORMAT_ARGB32, pic.width, pic.height, stride); 
-    texture = image.Texture.create_for_size(gl.GL_TEXTURE_2D, pic.width * pic.height, gl.GL_RGBA)
+    texture = pyglet.image.Texture.create_for_size(pyglet.gl.GL_TEXTURE_2D, pic.width * pic.height, pyglet.gl.GL_RGBA)
 
-    def update_surface(dt, surface):
+    def update(dt, surface):
         ctx = cairo.Context(surface)
         pic.draw_pic(ctx)
+
+    @window.event
+    def on_resize(width, height):
+        pass
+
+    def keyboard(c):
+        if c == 'q':
+            sys.exit(0)
+
+    @window.event
+    def on_key_press(symbol, modifiers):
+        keyboard(chr(symbol))
+
+    LEFT, MIDDLE, RIGHT = range(3)
+
+    BUTTONS = {
+        pyglet.window.mouse.LEFT:   LEFT,
+        pyglet.window.mouse.MIDDLE: MIDDLE,
+        pyglet.window.mouse.RIGHT:  RIGHT,
+    }
+
+    def mouse_button(button, pressed, x, y):
+        pass
+
+    def mouse_move(x1, y1, drag):
+        pass
+
+    @window.event
+    def on_mouse_press(x, y, button, modifiers):
+        mouse_button(BUTTONS[button], True, x, window.height-y)
+
+    @window.event
+    def on_mouse_release(x, y, button, modifiers):
+        mouse_button(BUTTONS[button], False, x, window.height-y)
+
+    @window.event
+    def on_mouse_motion(x, y, dx, dy):
+        mouse_move(x, window.height-y, False)
+
+    @window.event
+    def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
+        mouse_move(x, window.height-y, True)
 
     @window.event
     def on_draw():
         window.clear()
 
-        gl.glEnable(gl.GL_TEXTURE_2D)
+        pyglet.gl.glEnable(pyglet.gl.GL_TEXTURE_2D)
 
-        gl.glBindTexture(gl.GL_TEXTURE_2D, texture.id)
-        gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, pic.width, pic.height, 0, gl.GL_BGRA, gl.GL_UNSIGNED_BYTE, data)
+        pyglet.gl.glBindTexture(pyglet.gl.GL_TEXTURE_2D, texture.id)
+        pyglet.gl.glTexImage2D(pyglet.gl.GL_TEXTURE_2D, 0, pyglet.gl.GL_RGBA,
+            pic.width, pic.height, 0, pyglet.gl.GL_BGRA, pyglet.gl.GL_UNSIGNED_BYTE, data)
 
-        gl.glBegin(gl.GL_QUADS)
-        gl.glTexCoord2f(0.0, 1.0)
-        gl.glVertex2i(0, 0)
-        gl.glTexCoord2f(1.0, 1.0)
-        gl.glVertex2i(pic.width, 0)
-        gl.glTexCoord2f(1.0, 0.0)
-        gl.glVertex2i(pic.width, pic.height)
-        gl.glTexCoord2f(0.0, 0.0)
-        gl.glVertex2i(0, pic.height)
-        gl.glEnd()
+        pyglet.gl.glBegin(pyglet.gl.GL_QUADS)
+        pyglet.gl.glTexCoord2f(0.0, 1.0)
+        pyglet.gl.glVertex2i(0, 0)
+        pyglet.gl.glTexCoord2f(1.0, 1.0)
+        pyglet.gl.glVertex2i(pic.width, 0)
+        pyglet.gl.glTexCoord2f(1.0, 0.0)
+        pyglet.gl.glVertex2i(pic.width, pic.height)
+        pyglet.gl.glTexCoord2f(0.0, 0.0)
+        pyglet.gl.glVertex2i(0, pic.height)
+        pyglet.gl.glEnd()
 
         #text.draw()
 
         #print('FPS: %f' % clock.get_fps())
 
-    clock.schedule_interval(update_surface, 1/120.0, surface)
-    app.run()
+    midi_filename = 'Bach_Fugue_BWV578.mid'
+    if not midi_filename is None:
+        midi_file_player = MidiFileSoundPlayer(midi_filename, [pic])
+        midi_thread = Thread(target = midi_file_player.play)
+        midi_thread.start()
+    else:
+        midi_file_player = None
+        midi_thread = None
+
+    midi_input = RtMidiSoundPlayer([pic])
+
+    pyglet.clock.schedule_interval(update, 1/120.0, surface)
+    pyglet.app.run()
+
+    pyglet.clock.schedule_interval(update, 0, surface) # Specifying an interval of 0 prevents the function from being called again
+
+    if midi_thread:
+        midi_thread.join()
+        print("All threads finished")
 
 if __name__ == '__main__':
     main()
