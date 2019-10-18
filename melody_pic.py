02563de3bd2fe75da7981913054cb69059130137
Miriam Ruiz <miriam@debian.org>
2019-10-18 02:43:16 +0200

    Add proper colors to chords

--- melody_pic.py
+++ melody_pic.py
@@ -12,6 +12,15 @@ SCALE_MAJOR_DIATONIC = (1<<0) + (1<<2) + (1<<4) + (1<<6) + (1<<7) + (1<<9) + (1<
 
 NOTE_NAMES = ['I', 'ii', 'II', 'iii', 'III', 'IV', 'v', 'V', 'vi', 'VI', 'vii', 'VII']
 
+FIFTHS_NAMES = [
+    'Fb', 'Cb', 'Gb', 'Db', 'Ab', 'Eb', 'Bb',
+    'F',  'C',  'G',  'D',  'A',  'E',  'B',
+    'F#', 'C#', 'G#', 'D#', 'A#', 'E#', 'B#',
+    'Fx', 'Cx', 'Gx', 'Dx', 'Ax', 'Ex', 'Bx',
+]
+
+DIATONIC_SCALE_POS = [ 0, 7, 2, 9, 4, -1, 6, 1, 8, 3, 10, 5 ]
+
 NOTE_MIDI_A4 = 69
 NOTE_MIDI_C4 = 60
 
@@ -57,6 +66,40 @@ def hsv_to_rgb(hue, saturation=1., value=1.):
     elif hi == 5: r, g, b = v, p, q
     return r, g, b
 
+def lab_to_rgb(l, a, b):
+    y = (l + 16.) / 116.
+    x = a / 500. + y
+    z = y - b / 200.
+
+    x = 0.95047 * ((x * x * x) if (x * x * x > 0.008856) else ((x - 16./116.) / 7.787))
+    y = 1.00000 * ((y * y * y) if (y * y * y > 0.008856) else ((y - 16./116.) / 7.787))
+    z = 1.08883 * ((z * z * z) if (z * z * z > 0.008856) else ((z - 16./116.) / 7.787))
+
+    r = x *  3.2406 + y * -1.5372 + z * -0.4986
+    g = x * -0.9689 + y *  1.8758 + z *  0.0415
+    b = x *  0.0557 + y * -0.2040 + z *  1.0570
+
+    r = (1.055 * (r**(1./2.4)) - 0.055) if (r > 0.0031308) else (12.92 * r)
+    g = (1.055 * (g**(1./2.4)) - 0.055) if (g > 0.0031308) else (12.92 * g)
+    b = (1.055 * (b**(1./2.4)) - 0.055) if (b > 0.0031308) else (12.92 * b)
+
+    return [ max(0., min(1., r)), max(0., min(1., g)), max(0., min(1., b)) ]
+
+def rgb_to_lab(r, g, b):
+    r = ((r + 0.055) / 1.055)**2.4 if (r > 0.04045) else (r / 12.92);
+    g = ((g + 0.055) / 1.055)**2.4 if (g > 0.04045) else (g / 12.92);
+    b = ((b + 0.055) / 1.055)**2.4 if (b > 0.04045) else (b / 12.92);
+
+    x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+    y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+    z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+
+    x = x**(1./3.) if (x > 0.008856) else (7.787 * x) + 16/116.0;
+    y = y**(1./3.) if (y > 0.008856) else (7.787 * y) + 16/116.0;
+    z = z**(1./3.) if (z > 0.008856) else (7.787 * z) + 16/116.0;
+
+    return [(116. * y) - 16., 500. * (x - y), 200. * (y - z)]
+
 class MelodyPic:
     def __init__(self, D, scale=SCALE_MAJOR_DIATONIC, tonic=0):
         self.ctx = None
@@ -69,6 +112,7 @@ class MelodyPic:
         self.height = int(self.hstep * 27)
 
         self.root_note = 0
+        self.note_names = [FIFTHS_NAMES[8 + p] for p in DIATONIC_SCALE_POS]
         self.fifths_offset = 5
         self.notes_active = [ 0 ] * 128
         self.pitch_classes_active = [ 0 ] * 12
@@ -140,7 +184,7 @@ class MelodyPic:
             self.ctx.set_line_width(2.0)
         self.ctx.stroke()
 
-        label = NOTE_NAMES[note]
+        label = self.note_names[note]
         self.ctx.set_source_rgb(0.1, 0.1, 0.1)
         self.ctx.select_font_face("monospace", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
         self.ctx.set_font_size(10)
@@ -193,7 +237,7 @@ class MelodyPic:
                 self.ctx.arc(press_x, press_y, press_r, 0, 2. * math.pi)
                 self.ctx.fill()
 
-            label = NOTE_NAMES[n % 12]
+            label = self.note_names[n % 12]
             self.ctx.set_source_rgb(0., 0., 0.)
             self.ctx.select_font_face("monospace", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
             self.ctx.set_font_size(8)
@@ -230,13 +274,13 @@ class MelodyPic:
             press_r = self.BLACK_KEY_WIDTH / 2
 
             if is_pressed:
-                color = self.get_color_from_channel(channel, 0.5)
+                color = self.get_color_from_channel(channel, 1.0, 0.5)
                 self.ctx.set_source_rgb(*color)
                 self.ctx.arc(press_x, press_y, press_r, 0, 2. * math.pi)
                 self.ctx.fill()
 
-            label = NOTE_NAMES[n % 12]
-            self.ctx.set_source_rgb(0., 0., 0.)
+            label = self.note_names[n % 12]
+            self.ctx.set_source_rgb(1., 1., 1.)
             self.ctx.select_font_face("monospace", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
             self.ctx.set_font_size(6)
             text_extents = self.ctx.text_extents(str(label))
@@ -299,7 +343,7 @@ class MelodyPic:
                     chord_signature = chord_signatures[note]
                     if (pitch_classes & chord_signature) == chord_signature:
                         chords_found.append((note % 12, chord_name, chord_intervals, chord_color))
-                        #print("Found: {} on {} ({:04x} - {:04x} -> {:04x})".format(chord_name, NOTE_NAMES[note % 12],
+                        #print("Found: {} on {} ({:04x} - {:04x} -> {:04x})".format(chord_name, self.note_names[note % 12],
                         #    pitch_classes, chord_signature, pitch_classes & ~chord_signature))
                         pitch_classes &= ~chord_signature
 
@@ -315,7 +359,7 @@ class MelodyPic:
                 note = (self.root_note + n) % 12
                 for chord_root, chord_name, chord_intervals, chord_color in chords_found:
                     if note == chord_root % 12:
-                        print("Found: {} on {} ({})".format(chord_name, NOTE_NAMES[chord_root % 12], chord_intervals))
+                        #print("Found: {} on {} ({})".format(chord_name, self.note_names[chord_root % 12], chord_intervals))
                         pitch_classes_in_chord = set()
                         for d in chord_intervals:
                             if (n + d) >= -13 and (n + d) < 19 and (self.get_vpos_from_pitch_class(n + d) % 24) < 12:
@@ -324,6 +368,17 @@ class MelodyPic:
                                 pitch_classes_in_chord.add(n + d - 12)
 
                         if len(pitch_classes_in_chord) == len(chord_intervals):
+                            axis_lr = (sum(pitch_classes_in_chord) / len(pitch_classes_in_chord) - 11./3) / 13.5
+
+                            vdif = [(((c * 7 + self.fifths_offset) % 12) - self.fifths_offset - c / 7.) * 7. / 24. for c in pitch_classes_in_chord]
+                            axis_ud = sum(vdif) / len(vdif) * 3. / 5.
+
+                            nmaj = [(1./(n+1) if (j - i) == 4 else 0.) for n, (i, j) in enumerate(zip(chord_intervals[:-1], chord_intervals[1:]))]
+                            nmin = [(1./(n+1) if (j - i) == 3 else 0.) for n, (i, j) in enumerate(zip(chord_intervals[:-1], chord_intervals[1:]))]
+                            axis_mm = 5. * (sum(nmaj) - sum(nmin) ) / len(chord_intervals)
+
+                            chord_color = lab_to_rgb(75., (3 * axis_mm + axis_ud) * -20., axis_lr * 80.)
+
                             self.ctx.set_source_rgb(*chord_color)
                             self.ctx.set_line_width(50.0)
                             self.ctx.set_line_cap(cairo.LINE_CAP_ROUND)
@@ -398,7 +453,7 @@ class MelodyPic:
             self.ctx.set_line_width(1.0)
             self.ctx.stroke()
 
-        label = NOTE_NAMES[n % 12]
+        label = self.note_names[n % 12]
         self.ctx.set_source_rgb(0.1, 0.1, 0.1)
         self.ctx.select_font_face("monospace", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
         self.ctx.set_font_size(10)
